3374b1178fbe1c78bac1f5027929cc04849811b7
juancifuentes01 <juan.cifuentes@utp.edu.co>
2020-11-04 14:30:00 -0500

    rama1

--- archivoError.cpp
+++ archivoError.cpp
@@ -3,7 +3,6 @@ using namespace std;
 int main ()
 {
     cout<<"hola mundo"<<endl;
-    cout<<"1"<<endl;
-    cout<<"2"<<endl;
-    cout<<"3"<<endl;
+     cout<<"1"<<endl;
+      cout<<"2"<<endl;
 }
